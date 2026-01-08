977b25142c5e7e0fdedd26403ebb40b4d6d9b220
sarthakxsharma <jhundentertainment@gmail.com>
2026-01-08 22:39:58 +0530

    new verison

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.sayhi"
+    namespace = "com.piratexsharma.sayhi"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.sayhi"
+        applicationId = "com.piratexsharma.sayhi"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/sayhi/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sayhi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
